73aecf3a6360c95e31ae480f9ce50aa2dc3db40e
Emirhan Akpınar <59107578+fsystm@users.noreply.github.com>
2021-09-07 17:34:53 +0300

    Update [id].js

--- pages/series/[id].js
+++ pages/series/[id].js
@@ -48,7 +48,7 @@ export default function SitmovId({ post }) {
               {post.casts.map((cast) => (
                 <div className="castPics">
                   <Link href={cast.pic} >
-                  <a target="_" ><img className="castPic" src={cast.pic} alt={https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png} /></a>
+                  <a target="_" ><img className="castPic" src={cast.pic} alt="" /></a>
                   </Link>
                 </div>
               ))}
