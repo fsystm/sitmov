3a656bebc5d48e19398f8c5faf2647c571b6a87f
Emirhan Akpınar <59107578+fsystm@users.noreply.github.com>
2021-09-16 22:51:50 +0300

    Delete pages directory

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../styles/style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-slideshow-image/dist/styles.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
